7ae0aa041a38da56702a5561483164cffb0ccb54
EricYza <13681080795@163.com>
2025-10-07 16:36:17 -0400

    modify host

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="10.0.0.4", port=5000, debug=False)
+    app.run(host="20.48.202.167", port=5000, debug=False)
